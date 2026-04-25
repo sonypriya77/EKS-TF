784ef53f733c4fc04cfe6858f53d7cb166111bc3
sonypriya77 <sonypriya.cg@gmail.com>
2026-04-25 12:33:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket = "mario123bucket" # Replace with your actual S3 bucket name
+    bucket = "s3bucket.flm.eks" # Replace with your actual S3 bucket name
     key    = "Jenkins/terraform.tfstate"
-    region = "ap-south-1"
+    region = "us-east-1"
   }
 }
